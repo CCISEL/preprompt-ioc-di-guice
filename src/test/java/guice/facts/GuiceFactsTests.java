134b187350ee0a2625331ee37ff922a4e979e99d
Pedro Felix <pmhsfelix@gmail.com>
2011-02-03 18:09:27 +0000

    added more guice facts tests

--- src/test/java/guice/facts/GuiceFactsTests.java
+++ src/test/java/guice/facts/GuiceFactsTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.ConfigurationException;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -23,7 +24,7 @@ public class GuiceFactsTests {
 			_srv = srv;
 		}
 		public Service1 getService1(){ return _srv;}
-	}
+	}	
 	
 	@Test
 	public void guice_can_ctor_inject(){
@@ -36,4 +37,69 @@ public class GuiceFactsTests {
 		DependantWithCtorInjection dep = inj.getInstance(DependantWithCtorInjection.class);
 		assertEquals(Service1Impl1.class, dep.getService1().getClass());	
 	}
+	
+	static class DependantWithMethodInjection{		
+		private Service1 _srv;
+		
+		public DependantWithMethodInjection(){}
+		
+		@Inject
+		public void setService1(Service1 srv){
+			_srv = srv;
+		}
+		
+		public Service1 getService1(){ return _srv;}
+	}
+	
+	@Test
+	public void guice_can_method_inject(){
+		
+		Injector inj = Guice.createInjector(new AbstractModule(){
+			protected void configure(){
+				bind(Service1.class).to(Service1Impl1.class);				
+			}
+		});		
+		DependantWithMethodInjection dep = inj.getInstance(DependantWithMethodInjection.class);
+		assertEquals(Service1Impl1.class, dep.getService1().getClass());	
+	}
+	
+	@Test(expected=ConfigurationException.class)
+	public void method_injections_are_not_optional_by_default(){
+		
+		Injector inj = Guice.createInjector(new AbstractModule(){
+			protected void configure(){
+				// removed! bind(Service1.class).to(Service1Impl1.class);				
+			}
+		});		
+		DependantWithMethodInjection dep = inj.getInstance(DependantWithMethodInjection.class);
+		assertNull(dep.getService1());	
+	}	
+	
+	static class DependantWithOptionalMethodInjection{		
+		private Service1 _srv = null;
+		
+		public DependantWithOptionalMethodInjection(){}
+		
+		@Inject(optional=true)
+		public void setService1(Service1 srv){
+			_srv = srv;
+		}
+		
+		public Service1 getService1(){ return _srv;}
+	}	
+	
+	@Test
+	public void methods_injections_can_be_made_optional(){
+		
+		Injector inj = Guice.createInjector(new AbstractModule(){
+			protected void configure(){
+				// no binding!				
+			}
+		});		
+		DependantWithOptionalMethodInjection dep = 
+			inj.getInstance(DependantWithOptionalMethodInjection.class);
+		assertNull(dep.getService1());	
+	}
+	
+	
 }
